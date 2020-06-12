2669f479f02480d1427702196ea94d7da6f0b9ae
Mateo Cobos <mateocobosvega@gmail.com>
2020-06-12 21:39:12 +0200

    update fix register

--- server/controllers/signup.js
+++ server/controllers/signup.js
@@ -3,12 +3,31 @@ const _ = require('lodash')
 const User = require('../models/user')
 
 const signup = async (req, res) => {
-  const body = _.pick(req.body, ['firstname', 'lastname', 'email', 'password', 'password_confirm', 'address', 'birthday', 'gender', 'telephone', 'employee'])
+  const body = _.pick(req.body, [
+    'firstname',
+    'lastname',
+    'email',
+    'password',
+    'password_confirm',
+    'address',
+    'birthday',
+    'gender',
+    'telephone',
+    'employee',
+    'legals',
+    'newsletter'
+  ])
 
   // Controllare che la password corrisponda a password_confirm
   if (body.password !== body.password_confirm) {
     return res.status(403).json({
       message: 'Le password non corrispondono.'
+    })
+  }
+
+  if (body.legals === false) {
+    return res.status(403).json({
+      message: 'Non sono stati accettate le condizioni d\'uso.'
     })
   }
 
@@ -18,7 +37,7 @@ const signup = async (req, res) => {
     res.status(200).json(user)
   } catch (e) {
     res.status(500).json({
-      message: 'Impossibile creare l\'utente.'
+      message: "Impossibile creare l'utente."
     })
   }
 }
